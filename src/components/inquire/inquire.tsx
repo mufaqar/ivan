9928e3949c32d2ecc508a8f5c0b74a0406e05c31
syed zunair gillani <zunairgillani54@gmail.com>
2023-11-27 19:57:33 +0500

    hover

--- src/components/inquire/inquire.tsx
+++ src/components/inquire/inquire.tsx
@@ -6,7 +6,7 @@ const Inquire = () => {
   return (
     <section>
           <h2 className='font-pstime text-3xl md:text-4xl lg:text-6xl'>Longer form blurb about Ivan here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui faucibus in ornare quam. Ut faucibus pulvinar elementum integer. </h2>
-          <Link href="#" className='font-pstime hover:scale-105 text-2xl flex items-center gap-1 w-full justify-end mt-10 md:pr-10'>INQUIRE <BsArrowRight /></Link>
+          <Link href="#" className='font-pstime hover:text-gray text-2xl flex items-center gap-1 w-full justify-end mt-10 md:pr-10'>INQUIRE <BsArrowRight /></Link>
     </section>
   )
 }
