c2f7ecbde1968343984408fcf8a7b039fed6a2f9
mufaqar <mufaqar@gmail.com>
2023-12-07 14:37:34 +0500

    updateed

--- src/components/modelbox/ContactUs.tsx
+++ src/components/modelbox/ContactUs.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ImArrowRight2 } from "react-icons/im";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 export const ContactUs = ({closeModal}:any) => {
+
+     const {
+          register,
+          handleSubmit,
+          watch,
+          reset,
+          formState: { errors },
+      } = useForm<any>()
+      const [sending, setSending] = useState(false)
+  
+      const onSubmit: SubmitHandler<any> = (data: any) => {
+          setSending(true)
+          fetch('/api/email', {
+              method: 'POST',
+              headers: {
+                  Accept: 'application/json, text/plain, */*',
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(data),
+          }).then((res) => {
+              console.log('Response received');
+              if (res.status === 200) {
+                  console.log('Response succeeded!');
+                  alert('Message Successfully send.!');
+                  reset();
+                  setSending(false)
+              }
+          });
+      }
+
      return (
           <>
                <section className='h-screen relative w-full mx-auto bg-yellow p-8 md:p-16 pt-20 flex flex-col justify-center items-start'>
@@ -25,7 +56,7 @@ export const ContactUs = ({closeModal}:any) => {
                               <textarea name="" id="" className='block bg-transparent w-full py-1 2xl:py-2 outline-none border-b h-[80px] 2xl:h-[140px]' placeholder='Including budget, goals, aspirations, and more' />
                          </div>
                          <div className='mt-6 2xl:mt-10 flex justify-end'>
-                              <button className='flex items-center font-pstime hover:text-gray text-2xl gap-2' type='submit'>Send <ImArrowRight2 /></button>
+                              <button className='flex items-center font-pstime hover:text-gray text-2xl gap-2' type='submit'>{sending ? 'SENDING...' : `Send`} <ImArrowRight2 /></button>
                          </div>
                     </form>
                </section>
